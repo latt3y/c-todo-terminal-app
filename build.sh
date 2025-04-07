be064786b8797d4cf3f93f9bd5e4f51e479d95af
lattey <l4tt1haga@gmail.com>
2025-04-07 10:42:14 +0200

    adding std to build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 date
 echo "Building Todo App"
 
-gcc -O1 -Wall -Werror main.c ./lib/task.c ./lib/common.c ./lib/db.c -o main
+gcc -O1 -std=99 -Wall -Werror main.c ./lib/task.c ./lib/common.c ./lib/db.c -o main
 
 echo "Builded successfuly"
