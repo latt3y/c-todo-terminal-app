21c171ffdbfc5eceff54886c72c94ba817907319
Lati <l4tt1haga@gmail.com>
2025-05-21 21:01:26 +0200

    changes

--- headers/db.h
+++ headers/db.h
@@ -1,6 +1,8 @@
+#include <stdint.h>
+
 #define DB_NAME "./database/main.db"
 #define META_FILE_NAME "./database/indexes.db"
 
 void clear_file(void);
-void store_into_file(Task *tasks_buffer, unsigned int *mem_amount);
-void save_index(unsigned long *index);
+void store_into_file(Task* tasks_buffer, unsigned int *mem_amount);
+void save_index(uint64_t* index);
